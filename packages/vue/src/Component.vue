79757fb1142c64710b9df2d183cf77beb1daab6e
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-11-09 21:56:18 +0000

    Update package version

--- packages/vue/src/Component.vue
+++ packages/vue/src/Component.vue
@@ -1,19 +1,6 @@
 <script setup lang="ts">
-import { fooBar } from "@shipwright-sh/package-core";
-import { ref } from "vue";
-
-const count = ref(0);
-
-function increment() {
-	console.log(count.value);
-	count.value++;
-}
 </script>
 
 <template>
-	<div>
-		<p>Count: {{ count }}</p>
-		<p>FooBar: {{ fooBar(count) }}</p>
-	</div>
-	<button type="button" @click="increment">Increment</button>
+	TODO
 </template>
